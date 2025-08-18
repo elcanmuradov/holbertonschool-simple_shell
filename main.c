12311fe5bf316c878fd36a156eec7d0cee6d05c5
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-18 09:40:47 +0000

    3

--- main.c
+++ main.c
@@ -10,20 +10,19 @@ int main(int argc, char **argv)
 {
 char *command;
 char *prog_name = argv[0];
-
 (void)argc;
 while (1)
 {
 print_prompt();
 command = read_command();
-
 if (command == NULL)
 {
 if (isatty(STDIN_FILENO))
 printf("\n");
 break;
 }
-if (strlen(trim_whitespace(command)) == 0)
+trim_whitespace(command);
+if (strlen(command) == 0)
 {
 free(command);
 continue;
