1d5cbe22ba9f9c6ae22316e7e915ffe4a52ea224
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-18 09:51:02 +0000

    3

--- shell.c
+++ shell.c
@@ -7,8 +7,6 @@ void print_prompt(void)
 {
 if (isatty(STDIN_FILENO))
 printf("($) ");
-}
-
 /**
  * trim_whitespace - removes leading and trailing whitespace from string
  * @str: string to trim
@@ -18,6 +16,80 @@ char *trim_whitespace(char *str)
 {
 char *start = str;
 char *end;
+
+/* Skip leading whitespace */
+while (*start == ' ' || *start == '\t' || *start == '\n' || *start == '\r')
+start++;
+
+/* All spaces? */
+if (*start == '\0')
+{
+*str = '\0';
+return (str);
+}
+
+/* Trim trailing space */
+end = start + strlen(start) - 1;
+while (end > start && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r'))
+end--;
+
+/* Write new null terminator */
+*(end + 1) = '\0';
+
+/* Move trimmed content to beginning if necessary */
+if (start != str)
+{
+memmove(str, start, strlen(start) + 1);
+}
+
+return (str);
+}
+
+/**
+ * parse_command - parses command line into arguments
+ * @command: command line to parse
+ * Return: array of arguments (NULL terminated)
+ */
+char **parse_command(char *command)
+{
+char **argv;
+char *token;
+int argc = 0;
+int i;
+
+/* Count arguments */
+token = strtok(command, " \t");
+while (token != NULL)
+{
+argc++;
+token = strtok(NULL, " \t");
+}
+
+/* Allocate memory for argv array */
+argv = malloc(sizeof(char *) * (argc + 1));
+if (argv == NULL)
+return (NULL);
+
+/* Reset command string and parse again */
+token = strtok(command, " \t");
+for (i = 0; i < argc; i++)
+{
+argv[i] = token;
+token = strtok(NULL, " \t");
+}
+argv[argc] = NULL;
+
+return (argv);
+}
+/*
+ * @str: string to trim
+ * Return: pointer to trimmed string (modifies original)
+*/
+char *trim_whitespace(char *str)
+{
+char *start = str;
+char *end;
+
 while (*start == ' ' || *start == '\t' || *start == '\n' || *start == '\r')
 start++;
 if (*start == '\0')
@@ -69,18 +141,34 @@ void execute_command(char *command, char *prog_name)
 pid_t pid;
 int status;
 struct stat st;
+char **argv;
+char *command_copy;
 trim_whitespace(command);
 if (command[0] == '\0')
 return;
-if (stat(command, &st) == -1)
+command_copy = malloc(strlen(command) + 1);
+if (command_copy == NULL)
+return;
+strcpy(command_copy, command);
+argv = parse_command(command_copy);
+if (argv == NULL)
 {
-fprintf(stderr, "%s: 1: %s: not found\n", prog_name, command);
+free(command_copy);
+return;
+}
+if (stat(argv[0], &st) == -1)
+{
+fprintf(stderr, "%s: 1: %s: not found\n", prog_name, argv[0]);
+free(argv);
+free(command_copy);
 return;
 }
 
 if (!(st.st_mode & S_IXUSR))
 {
-fprintf(stderr, "%s: 1: %s: Permission denied\n", prog_name, command);
+fprintf(stderr, "%s: 1: %s: Permission denied\n", prog_name, argv[0]);
+free(argv);
+free(command_copy);
 return;
 }
 
@@ -88,15 +176,14 @@ pid = fork();
 if (pid == -1)
 {
 perror("fork");
+free(argv);
+free(command_copy);
 return;
 }
 
 if (pid == 0)
 {
-char *argv[2];
-argv[0] = command;
-argv[1] = NULL;
-execve(command, argv, environ);
+execve(argv[0], argv, environ);
 perror("execve");
 _exit(127);
 }
@@ -104,4 +191,7 @@ else
 {
 wait(&status);
 }
+
+free(argv);
+free(command_copy);
 }
