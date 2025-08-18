70d99be0245bdcaa1638a5e1bfdbff68a6b20026
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-18 10:00:00 +0000

    3

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include "shell.h"
 
-/**
- * main - entry point for simple shell
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success
- */
 int main(int argc, char **argv)
 {
 char *command;
 char *prog_name = argv[0];
 (void)argc;
+
 while (1)
 {
 print_prompt();
--- shell.c
+++ shell.c
@@ -1,42 +1,32 @@
 #include "shell.h"
 
-/**
- * print_prompt - prints the shell prompt
- */
 void print_prompt(void)
 {
 if (isatty(STDIN_FILENO))
 printf("($) ");
-/**
- * trim_whitespace - removes leading and trailing whitespace from string
- * @str: string to trim
- * Return: pointer to trimmed string (modifies original)
- */
+fflush(stdout);
+}
+
 char *trim_whitespace(char *str)
 {
 char *start = str;
 char *end;
 
-/* Skip leading whitespace */
 while (*start == ' ' || *start == '\t' || *start == '\n' || *start == '\r')
 start++;
 
-/* All spaces? */
 if (*start == '\0')
 {
 *str = '\0';
 return (str);
 }
 
-/* Trim trailing space */
 end = start + strlen(start) - 1;
 while (end > start && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r'))
 end--;
 
-/* Write new null terminator */
 *(end + 1) = '\0';
 
-/* Move trimmed content to beginning if necessary */
 if (start != str)
 {
 memmove(str, start, strlen(start) + 1);
@@ -45,74 +35,6 @@ memmove(str, start, strlen(start) + 1);
 return (str);
 }
 
-/**
- * parse_command - parses command line into arguments
- * @command: command line to parse
- * Return: array of arguments (NULL terminated)
- */
-char **parse_command(char *command)
-{
-char **argv;
-char *token;
-int argc = 0;
-int i;
-
-/* Count arguments */
-token = strtok(command, " \t");
-while (token != NULL)
-{
-argc++;
-token = strtok(NULL, " \t");
-}
-
-/* Allocate memory for argv array */
-argv = malloc(sizeof(char *) * (argc + 1));
-if (argv == NULL)
-return (NULL);
-
-/* Reset command string and parse again */
-token = strtok(command, " \t");
-for (i = 0; i < argc; i++)
-{
-argv[i] = token;
-token = strtok(NULL, " \t");
-}
-argv[argc] = NULL;
-
-return (argv);
-}
-/*
- * @str: string to trim
- * Return: pointer to trimmed string (modifies original)
-*/
-char *trim_whitespace(char *str)
-{
-char *start = str;
-char *end;
-
-while (*start == ' ' || *start == '\t' || *start == '\n' || *start == '\r')
-start++;
-if (*start == '\0')
-{
-*str = '\0';
-return (str);
-}
-end = start + strlen(start) - 1;
-while (end > start && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r'))
-end--;
-*(end + 1) = '\0';
-if (start != str)
-{
-memmove(str, start, strlen(start) + 1);
-}
-
-return (str);
-}
-
-/**
- * read_command - reads a command from stdin
- * Return: the command string or NULL on EOF
- */
 char *read_command(void)
 {
 char *line = NULL;
@@ -125,17 +47,13 @@ if (nread == -1)
 free(line);
 return (NULL);
 }
-if (line[nread - 1] == '\n')
+
+if (nread > 0 && line[nread - 1] == '\n')
 line[nread - 1] = '\0';
 
 return (line);
 }
 
-/**
- * execute_command - executes a command
- * @command: the command to execute
- * @prog_name: name of the shell program for error messages
- */
 void execute_command(char *command, char *prog_name)
 {
 pid_t pid;
@@ -143,19 +61,27 @@ int status;
 struct stat st;
 char **argv;
 char *command_copy;
+
 trim_whitespace(command);
+
 if (command[0] == '\0')
 return;
+
 command_copy = malloc(strlen(command) + 1);
 if (command_copy == NULL)
 return;
 strcpy(command_copy, command);
-argv = parse_command(command_copy);
+
+argv = malloc(sizeof(char *) * 2);
 if (argv == NULL)
 {
 free(command_copy);
 return;
 }
+
+argv[0] = command_copy;
+argv[1] = NULL;
+
 if (stat(argv[0], &st) == -1)
 {
 fprintf(stderr, "%s: 1: %s: not found\n", prog_name, argv[0]);
