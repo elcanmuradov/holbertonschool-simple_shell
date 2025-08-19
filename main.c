0a92752c2c72576f4704ee3f71c0e4942da4adaf
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-19 12:47:22 +0000

    3

--- main.c
+++ main.c
@@ -4,13 +4,19 @@ int main(int argc, char **argv)
 {
 char *command;
 char *prog_name = argv[0];
+char *token;
+char **args;
+int i, bufsize = 64;
 (void)argc;
 
 signal(SIGINT, SIG_IGN);
 
 while (1)
 {
-print_prompt();
+if (isatty(STDIN_FILENO))
+printf("($) ");
+fflush(stdout);
+
 command = read_command();
 if (!command)
 {
@@ -19,7 +25,50 @@ printf("\n");
 break;
 }
 
-execute_command(command, prog_name);
+trim_whitespace(command);
+if (command[0] == '\0')
+{
+free(command);
+continue;
+}
+
+args = malloc(bufsize * sizeof(char*));
+if (!args)
+{
+free(command);
+continue;
+}
+
+i = 0;
+token = strtok(command, " \t\r\n");
+while (token != NULL)
+{
+args[i] = token;
+i++;
+
+if (i >= bufsize)
+{
+bufsize += 64;
+args = realloc(args, bufsize * sizeof(char*));
+if (!args)
+{
+free(command);
+break;
+}
+}
+}
+args[i] = NULL;
+
+if (args[0] == NULL)
+{
+free(args);
+free(command);
+continue;
+}
+
+execute_command(args, prog_name);  
+
+free(args);
 free(command);
 }
 
--- shell.h
+++ shell.h
@@ -9,8 +9,11 @@
 #include <sys/stat.h>
 #include <signal.h>
 
+extern char **environ;
+
 void print_prompt(void);
 char *read_command(void);
-void execute_command(char *command, char *prog_name);
+char *trim_whitespace(char *str);
+void execute_command(char **args, char *prog_name);
 
 #endif
